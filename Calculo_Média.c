b0d7d429c1da31dd67d34acf3832bb552cd471fe
Eduardaregateiro <regateiroduda@msn.com>
2025-03-26 10:48:14 -0300

    inclusão de programa

---
+++ Calculo_Média.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int nota1, nota2, nota3;
+    int media;
+    
+    printf("\n");
+    printf("*** Programa de Cálculo de Média das Notas *** \n\n");
+
+    printf("Digite a sua primeira nota: \n");
+    scanf("%d", &nota1);
+
+    printf("Digite a sua segunda nota: \n");
+    scanf("%d", &nota2);
+
+    printf("Digite a sua terceira nota: \n");
+    scanf("%d", &nota3);
+
+    media = (nota1 + nota2 + nota3) /3;
+
+    printf ("A Média é: %d", media);
+    
+    
+    return 0;
+}
